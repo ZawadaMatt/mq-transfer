198a3dd184f1436c20ce60dae341dc8cc5ae8f9e
zawadamatt <mateusz.zawada10@gmail.com>
2021-04-16 21:23:34 +0200

    Initial commit

---
+++ src/main/java/com/github/zawadamatt/mqtransfer/MqTransferApplication.java
@@ -0,0 +1,13 @@
+package com.github.zawadamatt.mqtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zawadamatt/mqtransfer/MqTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zawadamatt.mqtransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
