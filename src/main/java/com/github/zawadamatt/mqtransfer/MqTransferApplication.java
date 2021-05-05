a827c03590ac8f04ec12751e3ca64fd221feda43
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-05-05 21:26:20 +0200

    Changes by mateu

--- src/main/java/com/github/zawadamatt/mqtransfer/MqTransferApplication.java
+++ src/main/java/com/github/zawadamatt/mqtransfer/MqTransferApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MqTransferApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MqTransferApplication.class, args);
-		System.out.println("done");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MqTransferApplication.class, args);
+        System.out.println("done");
+    }
 
 }
